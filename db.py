2b21880f9a531e445e827b6963b5aa493798e6d4
Lucas Vicentim <lucasvicentim0301@gmail.com>
2022-11-20 09:05:17 +0100

    DoC

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 # Dependencies
 import streamlit as st
-import pandas as pd
 
 from google.oauth2 import service_account
 from gsheetsdb import connect
@@ -23,23 +22,25 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-# Post / Writing Request to construct own DB at Google Sheets
-def postDataSOL(dataframe):
-    sa = gspread.service_account("credentials.json")
-    sh = sa.open("hackaTUM")
-    worksheetSOL = sh.get_worksheet(0)
-    st.write(dataframe.values.tolist())
-    worksheetSOL.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
-  
+# Post / Writing Request to construct own DB at Google Sheets  
 def postDataCS(dataframe):
+    """Example of post-alike request which fills own DB @GoogleAPI&Sheets
+    with a pairs:
+
+      0:  <Wallet | Credit Score>
+      . . . . 
+
+    The sad reality is that due to low transaction history of our tables the credit scoring algo
+    scores all on-chain transanction with credit of 0. The worst credit.... 
+
+    Nonetheless, the mathematics of the model seem accurate (revision is possible by uncommenting st.writes(...))
+    and out calculators and desmos graphical calculator double-checked our local results. Detailed doc on how
+    the algorithm works can be found @ the respective file.
+    """
     sa = gspread.service_account("credentials.json")
     sh = sa.open("hackaTUM")
     worksheetCS = sh.get_worksheet(1)
     worksheetCS.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
-
-# def postDataMA(dataframe):
-#     pass
-
     
 
 
